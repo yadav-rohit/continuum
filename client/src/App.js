06b357ade188e2057cf79b090a5fcdfeba94b229
JUHIE CHANDRA <juhiechandra@gmail.com>
2022-02-27 15:19:33 +0530

    1
    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import './App.css';
 import './Form.css';
 import Main from './Components/Main'
@@ -11,30 +11,43 @@ import Footer from './Components/Footer';
 
 
 import {
-  BrowserRouter,
-  Routes,
-  Route
+    BrowserRouter,
+    Routes,
+    Route
 } from "react-router-dom";
 
 function App() {
-  return (
-    <TransactionState>
-    <div className="flex flex-col md:min-h-screen  overflow-hidden  font-medium  bg-[#000000]">
-      <p> Hello</p>
-      <BrowserRouter>
-      <Header/>
-      <Routes>  
-      <Route exact path="/" element={<Main/>} />
-      {/* <Route exact path="/About" element={<About/>} /> */}
-      {/* <Route exact path="/DonationPage" element={<Dpage/>} /> */}
-      {/* <Route exact path="/Helper" element={<HelpsSection/>} /> */}
-      <Route exact path="/Contact" element={<Contact/>} />
-      </Routes>
-      </BrowserRouter>
-     <Footer/>
-    </div>
-    </TransactionState>
-  );
+    return ( <
+            TransactionState >
+            <
+            div className = "flex flex-col md:min-h-screen  overflow-hidden  font-medium  bg-[#000000]" >
+
+            <
+            BrowserRouter >
+            <
+            Header / >
+            <
+            Routes >
+            <
+            Route exact path = "/"
+            element = { < Main / > }
+            /> <
+            Route exact path = "/About"
+            element = { < About / > }
+            /> { / * < Route exact path = "/DonationPage"
+            element = { < Dpage / > }
+            /> */
+        } { /* <Route exact path="/Helper" element={<HelpsSection/>} /> */ } <
+        Route exact path = "/Contact"
+    element = { < Contact / > }
+    /> < /
+    Routes > <
+        /BrowserRouter> <
+    Footer / >
+        <
+        /div> < /
+        TransactionState >
+);
 }
 
 export default App;
